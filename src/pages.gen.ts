c0f5e265d7afa245367d59fed85c976bf535fd5e
Pierre Bertet <hi@bpier.re>
2026-01-16 16:34:20 +0000

    checks

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -4,29 +4,27 @@
 import type { PathsForPages, GetConfigResponse } from 'waku/router';
 
 // prettier-ignore
-import type { getConfig as File_BridgeBridgeIndex_getConfig } from './pages/(bridge)/bridge/index'
+import type { getConfig as File_BridgeBridgeIndex_getConfig } from './pages/(bridge)/bridge/index';
 // prettier-ignore
-import type { getConfig as File_Address_getConfig } from './pages/[address]'
+import type { getConfig as File_Address_getConfig } from './pages/[address]';
 // prettier-ignore
-import type { getConfig as File_Root_getConfig } from './pages/_root'
+import type { getConfig as File_Root_getConfig } from './pages/_root';
 // prettier-ignore
-import type { getConfig as File_Index_getConfig } from './pages/index'
+import type { getConfig as File_Index_getConfig } from './pages/index';
 
 // prettier-ignore
 type Page =
-	| ({ path: '/bridge' } & GetConfigResponse<
-			typeof File_BridgeBridgeIndex_getConfig
-	  >)
-	| ({ path: '/[address]' } & GetConfigResponse<typeof File_Address_getConfig>)
-	| ({ path: '/_root' } & GetConfigResponse<typeof File_Root_getConfig>)
-	| ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>)
+| ({ path: '/bridge' } & GetConfigResponse<typeof File_BridgeBridgeIndex_getConfig>)
+| ({ path: '/[address]' } & GetConfigResponse<typeof File_Address_getConfig>)
+| ({ path: '/_root' } & GetConfigResponse<typeof File_Root_getConfig>)
+| ({ path: '/' } & GetConfigResponse<typeof File_Index_getConfig>);
 
 // prettier-ignore
 declare module 'waku/router' {
-	interface RouteConfig {
-		paths: PathsForPages<Page>
-	}
-	interface CreatePagesConfig {
-		pages: Page
-	}
+  interface RouteConfig {
+    paths: PathsForPages<Page>;
+  }
+  interface CreatePagesConfig {
+    pages: Page;
+  }
 }
